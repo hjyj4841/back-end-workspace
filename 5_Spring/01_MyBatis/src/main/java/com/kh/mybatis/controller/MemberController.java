346b96c372fae17ba6a019c7a0baa1989180da64
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-08-01 14:19:32 +0900

    Spring_mybatis문법

--- 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
+++ 5_Spring/01_MyBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
@@ -1,12 +1,16 @@
 package com.kh.mybatis.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.mybatis.model.dto.SearchDTO;
 import com.kh.mybatis.model.vo.Member;
 import com.kh.mybatis.service.MemberService;
 
@@ -70,6 +74,18 @@ public class MemberController {
 		if(vo.getName() == null) vo.setName(member.getName());
 		
 		session.setAttribute("vo", vo);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/search")
+	public String search(SearchDTO dto, Model model) {
+		model.addAttribute("list", service.search(dto));
+		return "index";
+	}
+	
+	@PostMapping("/delete")
+	public String delete(@RequestParam(name="idList", required=false) List<String> idList) {
+		if(idList != null) service.delete(idList);
 		return "redirect:/";
 	}
 	
