68eb0b61ddb29e190f8496b13ce6ceb12e8b8cbb
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-09-25 17:54:11 +0900

    Spring_REST API

---
+++ 5_Spring/06_REST/src/main/java/com/kh/upload/service/BoardService.java
@@ -0,0 +1,54 @@
+package com.kh.upload.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.upload.model.vo.Board;
+import com.kh.upload.model.vo.Paging;
+
+import mapper.BoardMapper;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	public void createBoard(Board board) {
+		mapper.createBoard(board);
+	}
+	
+	public List<Board> showList(Paging paging){
+		
+		/*
+		 * limit가 10인 경우
+		 * page = 1 -> offset = 0
+		 * page = 2 -> offset = 10
+		 * page = 3 -> offset = 20 ...
+		 * 
+		 * offset = (page - 1) * limit
+		 */
+		paging.setOffset((paging.getPage() - 1) * paging.getLimit());
+		
+		return mapper.showList(paging);
+	}
+	
+	public int total() {
+		return mapper.total();
+	}
+	
+	public Board view(int no) {
+		return mapper.view(no);
+	}
+	
+	public void update(Board board) {
+		mapper.update(board);
+	}
+	
+	public void delete(int no) {
+		mapper.delete(no);
+	}
+	
+}
---
+++ 5_Spring/06_REST/src/main/java/mapper/BoardMapper.java
@@ -0,0 +1,18 @@
+package mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.upload.model.vo.Board;
+import com.kh.upload.model.vo.Paging;
+
+@Mapper
+public interface BoardMapper {
+	void createBoard(Board board);
+	List<Board> showList(Paging paging);
+	Board view(int no);
+	void update(Board board);
+	void delete(int no);
+	int total();
+}
