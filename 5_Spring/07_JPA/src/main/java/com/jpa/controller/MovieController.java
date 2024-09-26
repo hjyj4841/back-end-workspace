49fc7452ad5098e696e040c158841b364a39fe68
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-09-26 13:37:17 +0900

    JPA_CrossOrigin 설정 추가

--- 5_Spring/07_JPA/src/main/java/com/jpa/controller/MovieController.java
+++ 5_Spring/07_JPA/src/main/java/com/jpa/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.jpa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.jpa.service.MovieService;
 
 @RestController
 @RequestMapping("/api/*")
+@CrossOrigin(origins = {"*"}, maxAge=6000) // CORS error
 public class MovieController {
 	
 	@Autowired
